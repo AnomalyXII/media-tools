05d9e70260500b41691e8b8756deff7231da7f0d
anomalyxii <nom@anomaly-xii.net>
2017-02-19 20:01:34 +0000

    Add interfaces for reading AudioFiles

---
+++ media-tools-api/src/main/java/net/anomalyxii/mediatools/api/readers/MetadataReader.java
@@ -0,0 +1,20 @@
+package net.anomalyxii.mediatools.api.readers;
+
+import net.anomalyxii.mediatools.api.models.Metadata;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Created by Anomaly on 17/04/2016.
+ */
+public interface MetadataReader {
+
+    // ******************************
+    // Interface Methods
+    // ******************************
+
+    Metadata read(URI uri) throws IOException;
+
+
+}
